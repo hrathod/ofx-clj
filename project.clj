481d33d198ebaa65b94e41503fdb868de9fba1cf
Himanshu Rathod <himanshu.rathod@gmail.com>
2013-08-15 11:04:51 -0400

    updted OFX4J version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject ofx-clj "0.1"
   :description "A Clojure wrapper for the OFX4J library for parsing OFX documents."
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [net.sf.ofx4j/ofx4j "1.5"]])
+                 [net.sf.ofx4j/ofx4j "1.6"]])
